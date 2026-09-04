ee918419c6da25168316218e20db7f3b1ae35193
daviti21 <dmodebadze63@gmail.com>
2026-09-04 18:06:07 +0400

    update Region Search and Country Detail css

--- src/app/home/search/search.ts
+++ src/app/home/search/search.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, input, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CountryService } from '../../services/country.service';
 
 @Component({
@@ -7,22 +7,29 @@ import { CountryService } from '../../services/country.service';
   styleUrl: './search.css',
   templateUrl: './search.html',
 })
-export class Search {
-    isActive = signal(false);
+export class Search  {
+  isActive = signal(false);
+  regionString = signal('Region');
+regionActive = signal(false);
   optionActive() {
     this.isActive.update((v) => !v);
   }
   countryService = inject(CountryService);
-  searchCountry(event: Event){
+  searchCountry(event: Event) {
     const value = (event.target as HTMLInputElement).value;
 
     this.countryService.searchValue.set(value);
-
-   }
-  searchedRegion(region: string){
-
+  }
+  searchedRegion(region: string) {
     this.countryService.selectedRegion.set(region);
+  }
+  regionStringValue() {
+       this.regionActive.update((v) => !v);
+       if(this.regionActive()){
+         this.regionString.set(this.countryService.selectedRegion());
+       }else {
+         this.regionString.set("Region");
+       }
 
   }
-
- }
+}
