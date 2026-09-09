4f2465f3ef33afe68df7e05412bf0894ffc90907
daviti21 <dmodebadze63@gmail.com>
2026-09-09 22:33:33 +0400

    update app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CountryDetails } from './country-details/country-details';
+
 
 export const routes: Routes = [
   {
