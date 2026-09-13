90cfd7d8299b55a4d5db2b5d920e1ad5841c0f5b
daviti21 <dmodebadze63@gmail.com>
2026-09-13 14:12:16 +0400

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,15 +8,15 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: '**',
-    redirectTo: '/Home'
-  },
-  {
     path: 'Home',
     loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'CountryDetail/:name',
     loadComponent: () => import('./country-details/country-details').then((m) => m.CountryDetails),
+  },
+  {
+    path: '**',
+    redirectTo: '/Home',
   },
 ];
